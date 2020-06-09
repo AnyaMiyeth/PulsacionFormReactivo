bf2474150e81101f78f00236bd0933f98a4f5ccc
anyamiyeth <anyamiyeth@byasystems.com.co>
2020-06-09 00:54:58 -0500

    OK

--- Entity/Persona.cs
+++ Entity/Persona.cs
@@ -7,6 +7,7 @@ namespace Entity
         [Key]
         public string Identificacion { get; set; }
         
+
         public string Nombre { get; set; }
         public int Edad { get; set; }
         public string Sexo { get; set; }
